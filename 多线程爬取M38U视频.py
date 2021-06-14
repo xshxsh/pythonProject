5835daf06b07053c21ef003c7b2fcd29319c2202
xshxsh <2477568542@qq.com>
2021-06-14 15:23:54 +0800

    多线程爬取M38U视频

--- 多线程爬取M38U视频.py
+++ 多线程爬取M38U视频.py
@@ -1,8 +1,7 @@
-import aiofiles
+import threading
 import requests
 import re
-import asyncio
-import aiohttp
+from concurrent.futures import ThreadPoolExecutor
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36"
@@ -15,10 +14,8 @@ def download_m3u8():
     url = 'https://www.91kanju.com/vod-play/59608-1-1.html'
     resp = requests.get(url, headers=headers)
     resp.encoding = 'utf-8'
-    # print(resp.text)
     reg = re.compile(r'url: \'(?P<m3u8_url>.*?)\',', re.S)
     m3u8_url = reg.search(resp.text).group('m3u8_url')
-    # print(m3u8_url)
 
     # 下载m3u8文件
     m3u8_resp = requests.get(m3u8_url, headers=headers)
@@ -32,7 +29,7 @@ def download_m3u8():
 
 
 # 解析m3u8文件
-async def parse_m3u8():
+def parse_m3u8():
     tasks = []
     with open('file/孤星第二季01.m3u8', mode='r', encoding='utf-8') as f:
         for i, line in enumerate(f):
@@ -42,22 +39,23 @@ async def parse_m3u8():
             # 测试，只下载几个
             if i == 20:
                 break
-            tasks.append(asyncio.create_task(download_video(line.split('/')[-1], line)))
-    await asyncio.wait(tasks)
+            tasks.append((line.split('/')[-1], line))
+    # 开启10个线程同时下载
+    with ThreadPoolExecutor(10) as t:
+        for item in tasks:
+            t.submit(download_video, item[0], item[1])
 
 
 # 下载视频片段
-async def download_video(name, url):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, headers=headers) as resp:
-            async with aiofiles.open(f'file/{name}.ts', mode='wb') as f:
-                await f.write(await resp.content.read())
-                print(f'下载完{name}.ts')
+def download_video(name, url):
+    resp = requests.get(url, headers=headers)
+    resp.encoding = 'utf-8'
+    with open(f'file/{name}.ts', mode='wb') as f:
+        f.write(resp.content)
+        print(f'当前线程：{threading.currentThread().name}，下载完{name}.ts')
 
 
 if __name__ == '__main__':
     download_m3u8()
-
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(parse_m3u8())
+    parse_m3u8()
     print('over')
