f95e981ff29f7452924601f9135f65043de8b1ad
谢仕海 <xieshihai@aulton.com>
2024-07-17 14:09:14 +0800

    json转Excel

---
+++ excel/JsonToExcel.py
@@ -0,0 +1,16 @@
+import json
+import pandas as pd
+import openpyxl
+
+if __name__ == '__main__':
+    # 读取本地json文件
+    with open('aulton-application.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    # 将json数据转换为DataFrame
+    df = pd.DataFrame(data)
+
+    # 将DataFrame写入Excel文件
+    df.to_excel('output.xlsx', index=False)
+
+    print("JSON数据已成功转换为Excel文件")
